4005d65dc302d4385c2d7b4e39a1ac80cb5637cd
SASAKI Shigeo <sasaki-shigeo@acm.org>
2022-08-27 00:12:51 +0900

    Refactoring array version of the list of primes; Test code added (trial version)

--- primes.jl
+++ primes.jl
@@ -1,4 +1,12 @@
-primesTo10 = [2,3,5,7]
+#
+# To load this file, type following
+#
+# ```sh
+# include("primes.jl")
+# ```
+#
+
+primesTo10 = [2, 3, 5, 7]
 
 function isOddPrimeBy(n::Integer; by=[3,5,7])::Bool
     @assert n % 2 != 0 "non even assumed: $n"
@@ -7,33 +15,114 @@ function isOddPrimeBy(n::Integer; by=[3,5,7])::Bool
     end
 end
 
-primes5bits = [2, 3, 5, 7, 11, 13, 17, 19, 23, 39, 31]
+primes5bits = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
 
-primesTo100 = vcat(primesTo10,
-                   [ n for n in 11:2:99
-                       if isOddPrimeBy(n, by = [3,5,7])])
+function oddSieve(odds; by = primes5bits)
+    [ n  for n in odds
+         if all(by) do p
+             n % p != 0
+         end ]
+end
 
-primesTo1000 = vcat(primes5bits,
-                    [ n for n in 37:2:999
-                        if isOddPrimeBy(n, by = @view primes5bits[begin+1:end])
-                    ])
+#
+# Note: Let u and v be vectors,
+# [ u; v ] means vcat(u, v)
+# 
 
-primesTo10000 = vcat(primesTo100,
-                     [ n for n in 101:2:9999
-                         if isOddPrimeBy(n, by = @view primesTo100[begin+1:end])
-                     ])
+primesTo100 = [ primesTo10; oddSieve(11:2:99, by = [3, 5, 7]) ]
 
-primesTo1000000 = vcat(primesTo1000,
-                       [ n for n in 1001:2:999999
-                           if isOddPrimeBy(n, by = @view primesTo1000[begin+1:end])
-                       ])                
+primesTo1000 = [ primes5bits;
+                 oddSieve(37:2:999, by = @view primes5bits[begin+1:end]) ]
 
-function isPrimeBy(n::Integer; by::Vector{Int} = primesTo10000)::Bool
-    all(by) do p
-        n % p != 0
+primesTo10000 = [ primesTo100;
+                  oddSieve(101:2:9999, by = @view primesTo100[begin+1:end]) ]
+
+# primesTo1000000 = [ primesTo1000;
+#                     oddSieve(1001:2:999999, by = @view primesTo1000[begin+1:end]) ]
+
+
+function isPrime(n::Integer; by = primesTo10000)
+    if (n <= last(by))
+        n in by
+    else
+        all(by) do p
+            n % p != 0
+        end
     end
 end
 
+using Base.Iterators
+
+#
+# Lists of primes numbers
+#
+# The constructor creates a list of primes numbers
+# in BitArray (bit by bit representation if the index is prime)
+# by Eratosthenes sieve algorithm.
+#
+# The most time-consuming phase of the algorithm is checking if
+# N is divisible by 2 (and 3).  It is a nice idea to buid 
+# the primes candidate list without evens > 2, initially.
+# Fortunately, its bit pattern is quite simple. See below.
+#
+# BitArray().chunks is the inner representation of BitArray()
+#
+# oddsAndTwo =  { 2, 3, 5, 7, 9, 11, 13, ... }
+# Its bitmap is
+#
+# | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 |...|63 |64 ||65 |66 |...|128|
+# | 0 | 1 | 1 | 0 | 1 | 0 | 1 | 0 | 1 |...| 1 | 0 || 1 | 0 |...|  0|
+#
+# Its reverse ordered:
+#
+# |128|127|...|66|65||64|63|...| 8| 7| 6| 5| 4| 3| 2| 1|
+# | 0 | 1 |...| 0| 1|| 0| 1|...| 0| 1| 0| 1| 0| 1| 1| 0|
+#
+# Its hexadecimal reporesentation:
+# [0x55...55, 0x55..56]
+#
+
+struct PrimesBitmap
+    bitmap:: BitVector
+
+    function PrimesBitmap(size)
+        this = new(falses(size))
+        fill!(this.bitmap.chunks,
+              0x5555_5555_5555_5555_5555_5555_5555_5555 % typeof(this.bitmap.chunks[1]))
+        this.bitmap.chunks[begin] =
+            0x5555_5555_5555_5555_5555_5555_5555_5556 % typeof(this.bitmap.chunks[1])
+
+        for p in 3:2:isqrt(size)
+            if this.bitmap[p]   # when P is a prime number.
+                # p^2, p^2 + p, p^2 + 2p, ... are not prime.
+                # Note: p^2 + p, p^2 + 3p, ... are not need to sieve
+                # since even are p^2 + p = p(p + 1) and so on.
+                # Sieve only p^2, p^2 + 2p, p^2 + 4p, ...
+                for q in p^2:2p:lastindex(this.bitmap)
+                    this.bitmap[q] = false
+                end
+            end
+        end
+        return this
+    end
+end
+
+function isPrime(n::Integer, by::PrimesBitmap)
+    if n <= lastindex(by)
+        by.bitmap[n]
+    else
+        all(by) do p
+            n % p != 0
+        end
+    end
+end
+
+Base.collect(primes::PrimesBitmap) = filter(p->primes.bitmap[p],
+                                            eachindex(primes.bitmap))
+
+Base.count(primes::PrimesBitmap) = count(primes.bitmap)
+
+#= primesBitmap and is sieve() are obsolete
 if ! @isdefined primesBitmap
     primesBitmap = trues(0xffffff)
 end
@@ -58,11 +147,28 @@ end
 
 #sieve()
 
+=#
+
 struct OddBitmap <: AbstractVector{Bool}
     bitmap::BitVector
 
     function OddBitmap(n::Int)::OddBitmap
         bitmap = trues(n)
+    end
+end
+
+#
+# | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | † the index of the internal bitmap
+# | 3 | 5 | 7 | 9 | 11| 13| 15| 17| 19| ‡ the index of PrimesBitmap2
+# | 1 | 1 | 1 | 0 | 1 | 1 | 0 | 1 | 1 |   whether is a prime number
+#
+struct PrimesBitmap2
+    bitmap::BitVector
+
+    function PrimesBitmap2(limit::Int)
+        this = new(trues((limit - 1) ÷ 2))
+
+        this
     end
 end
 
@@ -76,31 +182,42 @@ end
 # outside == 2 * inside + 1
 # inside  == (outside - 1) ÷ 2
 #
-function getindex(xs::OddBitmap, k::Int)
-    @assert (k & 0x01) == 1 && k >= 3
-    xs.bitmap[(k-1) ÷ 2]
+function Base.getindex(xs::PrimesBitmap2, k::Int)
+    if k == 1
+        false
+    elseif k == 2
+        true
+    else
+        @assert (k & one(k)) == one(k) && k >= 3
+        xs.bitmap[(k-1) ÷ 2]
+    end
 end
 
-function setindex!(xs::OddBitmap, val, k::Int)
-    @assert (k & 0x01) == 1 && k >= 3
+function Base.setindex!(xs::PrimesBitmap2, val, k::Int)
+    @assert (k & one(k)) == one(k) && k >= 3
     xs.bitmap[(k-1) ÷ 2] = val
 end
 
-function size(xs::OddBitmap)
-    return size(xs.bitmap)
-end
+Base.firstindex(xs::PrimesBitmap2) = 3
+Base.lastindex(xs::PrimesBitmap2) = 2 * lastindex(xs.bitmap) + 1
+Base.keys(xs::PrimesBitmap2) = firstindex(xs):2:lastindex(xs)
+Base.count(xs::PrimesBitmap2) = count(k -> xs.bitmap[k], keys(xs.bitmap))
 
-# this definition overrides functions axes and eachindex but not size
-function axes(xs::OddBitmap)
-    map(n->(3:2:(2n+1)), size(xs))
-end
 
-function show(io::IO, ::MIME"text/plain", xs::OddBitmap)
+function show(io::IO, ::MIME"text/plain", xs::PrimesBitmap2)
     summary(io, xs)
     print(io, xs.bitmap)
 end
 
-        
+function sieve(xs::PrimesBitmap2)
+    for p in 3:2:isqrt(lastindex(xs))
+        if xs[p]
+            for q in p^2:2p:lastindex(xs)
+                xs[q] = false
+            end
+        end
+    end
+end
 
 #
 # oddPrimesBitmap[1] means 3 is prime.
@@ -146,7 +263,7 @@ end
 #
 function intFactor(n::Integer)
     if n == 0
-        throw(ArgumentError("intFactor expects non-zero integer but 0"))
+        throw(ArgumentError("intFactor expects non-zero integer"))
     end
 
     factors = Vector{Integer}()
@@ -203,6 +320,11 @@ function intFactor(n::Integer)
     if d != 0
         factors[2] = d
     end
+
+    if n == 1
+        return factors
+    end
+
     @assert n % 2 != 0
 
     for ix in eachindex(oddPrimesBitmap)
@@ -340,6 +462,15 @@ function mersennePrimesByLucasLehmer()
     end
 end
 
-function isPrimeByMillerRabin(p::Integer)::Bool
-    d = p - 1
-end
+
+#
+# Test Routine
+#
+
+using Test
+
+@test length(primesTo10) == 4
+@test length(primes5bits) == 11
+@test length(primesTo100) == 25
+@test length(primesTo1000) == 168
+@test length(primesTo10000) == 1229
